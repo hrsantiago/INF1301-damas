88465ff5a390341897731803d0899314d6283c31
mmresende <marcelomresende@globo.com>
2014-05-01 23:52:08 -0300

    mais um batch e leiame

--- Batches/TestaTudo.bat
+++ Batches/TestaTudo.bat
@@ -3,11 +3,11 @@ REM executa os testes de lista, peca e tabuleiro
 
 cls
 
-if exist ..\ferramnt\exbestat goto estatistica
+if exist ..\ferramnt\exbestat.exe goto estatistica
 
 ..\produto\TESTELISTA		/s..\scripts\TesteLista		/l..\produto\TesteLista		
 ..\produto\TESTEPECA  		/s..\scripts\Testepeca    	/l..\produto\TestePeca  	
-..\produto\TESTETABULEIRO  	/s..\scripts\Testetabuleiro	/l..\produto\TesteTabuleiro   	
+..\produto\TESTETABULEIRO  	/s..\scripts\Testetabuleiro	/l..\produto\TesteTabuleiro   
 
 goto sai
 
